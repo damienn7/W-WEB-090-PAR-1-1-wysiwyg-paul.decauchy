f43eb76188d07f68c19b3f95db893e5eef88c7e6
pierre palmer <pierre.palmer@epitech.eu>
2023-02-04 18:07:18 +0100

    envoi bold/unbold

--- module.js
+++ module.js
@@ -1,6 +1,12 @@
 export function bold(textElement){
     return function (){
-        wrapText(textElement, "<b>", "</b>");
+   
+
+        // if(match){
+        //     unWrapText()
+        // } else {
+            wrapText(textElement, "<b>", "</b>");
+        // }
     };
 }
 
@@ -10,26 +16,25 @@ function wrapText(textElement, open, close){
     let text = textElement.value;
     var selectStart = textElement.selectionStart;
     var selectEnd = textElement.selectionEnd;
+    var finalResult = "";
 
     // let offsetStart = text.indexOf(select.baseNode.parentElement.innerHTML);
     // let selectStart = offsetStart + select.anchorOffset;
     // let selectEnd = offsetStart + select.focusOffset
-
     let entryStart = text.substring(0, selectStart);
     let entryEnd = text.substring(selectEnd, text.length);
-    let finalResult = entryStart + open + select + close + entryEnd;
-    textElement.innerHTML = finalResult;
-    // return finalResult;
+    let checkStart = text.substring(selectStart-open.length,selectStart);
+    let checkEnd = text.substring(selectEnd , selectEnd + close.length);
+    let check = checkStart + select + checkEnd;
+    let regex = new RegExp(`^${open}.*${close}$`);
+    if(regex.test(check) === true){
+        let entryStartT = text.substring(0, selectStart-open.length);
+        let entryEndT = text.substring(selectEnd + close.length, text.length );
+        finalResult = entryStartT + select + entryEndT
+    } else {    
+        finalResult = entryStart + open + select + close + entryEnd;
+        // return finalResult;
+    }
+    textElement.value = finalResult;
 }
 
-
-//     {select.baseNode.parentElement.outerHTML
-//         var desc = $("#textarea").val();
-//         var selStart = $("#textarea")[0].selectionStart;
-//         var selEnd = $("#textarea")[0].selectionEnd;
-//         var text = desc.substring(selStart, selEnd);
-//         var before = desc.substring(desc, selStart);
-//         var after = desc.substring(selEnd, desc.length);
-//         $("#textarea").val(before + openTAG + text + closeTAG + after);
-//         $("#editeur").html($("#textarea").val());
-//     }
