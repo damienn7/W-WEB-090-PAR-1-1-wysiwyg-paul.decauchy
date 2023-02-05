09425dc37f3c2a510ef7ff09990e05bcdc2fc58e
pierre palmer <pierre.palmer@epitech.eu>
2023-02-05 11:30:43 +0100

    envoi suite des fonctionnalité étape 3

--- module.js
+++ module.js
@@ -1,14 +1,82 @@
 export function bold(textElement){
     return function (){
-   
-
-        // if(match){
-        //     unWrapText()
-        // } else {
             wrapText(textElement, "<b>", "</b>");
-        // }
     };
 }
+
+export function italic(textElement){
+    return function (){
+            wrapText(textElement, "<i>", "</i>");
+    };
+}
+
+export function underline(textElement){
+    return function (){
+            wrapText(textElement, "<u>", "</u>");
+    };
+}
+
+export function strike(textElement){
+    return function (){
+            wrapText(textElement, "<s>", "</s>");
+    };
+}
+
+export function link(textElement){
+    return function (){
+            // en travail
+            wrapText(textElement, `<a href="" >`, "</a>");
+    };
+}
+
+export function alignRight(textElement){
+    return function (){
+            wrapText(textElement, `<div class="right">`, "</div>");
+    };
+}
+
+export function alignLeft(textElement){
+    return function (){
+            wrapText(textElement, `<div class="left">`, "</div>");
+    };
+}
+
+export function alignCenter(textElement){
+    return function (){
+            wrapText(textElement, `<div class="center">`, "</div>");
+    };
+}
+
+export function justify(textElement){
+    return function (){
+            wrapText(textElement, `<div class="justify">`, "</div>");
+    };
+}
+
+export function orange(textElement){
+    return function (){
+        wrapText(textElement, `<span class="orange">`, "</span>");
+    };
+}
+
+export function purple(textElement){
+    return function (){
+        wrapText(textElement, `<span class="purple">`, "</span>");
+    };
+}
+
+export function red(textElement){
+    return function (){
+        wrapText(textElement, `<span class="red">`, "</span>");
+    };
+}
+
+export function green(textElement){
+    return function (){
+            wrapText(textElement, `<span class="green">`, "</span>");
+    };
+}
+
 
 function wrapText(textElement, open, close){
     let select = document.getSelection();
--- my_wysiwyg.js
+++ my_wysiwyg.js
@@ -1,6 +1,35 @@
-import { bold } from './module.js';
+import { bold, italic,underline, strike, orange, purple, green, red, alignLeft, alignRight, alignCenter, justify } from './module.js';
 let text = document.getElementById("textZone");
 let textarea = document.getElementById("textarea");
 let bBold = document.getElementById("bold");
+let bItalic = document.getElementById("italic");
+let bSurlign = document.getElementById("underline");
+let bStrike = document.getElementById("strike");
+let bOrange = document.getElementById("orange");
+let bPurple = document.getElementById("purple");
+let bGreen = document.getElementById("green");
+let bRed = document.getElementById("red");
+let bLeft = document.getElementById("left");
+let bRight = document.getElementById("right");
+let bCenter = document.getElementById("center");
+let bJustify = document.getElementById("justify");
+// let bBold = document.getElementById("bold");
+// let bBold = document.getElementById("bold");
+// let bBold = document.getElementById("bold");
 
 bBold.onclick = bold(textarea);
+bItalic.onclick = italic(textarea);
+bSurlign.onclick = underline(textarea);
+bStrike.onclick = strike(textarea);
+bOrange.onclick = orange(textarea);
+bPurple.onclick = purple(textarea);
+bGreen.onclick = green(textarea);
+bRed.onclick = red(textarea);
+bLeft.onclick = alignLeft(textarea);
+bRight.onclick = alignRight(textarea);
+bCenter.onclick = alignCenter(textarea);
+bJustify.onclick = justify(textarea);
+
+function update(){
+    text.innerHTML = textarea.value;
+}
